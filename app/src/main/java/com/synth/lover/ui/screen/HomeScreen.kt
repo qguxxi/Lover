72374f17c6e1e68401bf1d8692b0c3b1c384b0c2
Duc Hoang Manh <jandyz.dev@gmail.com>
2024-12-05 03:26:32 +0700

    add icon launcher

--- app/src/main/java/com/synth/lover/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/synth/lover/ui/screen/HomeScreen.kt
@@ -1,2 +1,21 @@
 package com.synth.lover.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+@Composable
+fun HomeScreen(
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        
+        Text(text = "Hello World")
+        
+    }
+    
+}
